589b115f2442f0d2147e798a2b3bf12a0a61d1dc
Orlov Ilya <EagleCanFly>
2020-06-21 12:26:58 +0300

    Updates

---
+++ precache-manifest.a807091b2ef16b81d3375982e6b2dd38.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9dd5f90ad3e9205ccdb2fc4e3199b34",
+    "url": "/github-repository-viewer/index.html"
+  },
+  {
+    "revision": "56af2cbdfa72c2149349",
+    "url": "/github-repository-viewer/static/css/main.fc733940.chunk.css"
+  },
+  {
+    "revision": "d788a5203f84bcbb9a4f",
+    "url": "/github-repository-viewer/static/js/2.72eb0534.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/github-repository-viewer/static/js/2.72eb0534.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56af2cbdfa72c2149349",
+    "url": "/github-repository-viewer/static/js/main.5434ea62.chunk.js"
+  },
+  {
+    "revision": "a5ffde83bd7a2f303ec5",
+    "url": "/github-repository-viewer/static/js/runtime-main.d3633d97.js"
+  },
+  {
+    "revision": "41e8dead03fb979ecc23b8dfb0fef627",
+    "url": "/github-repository-viewer/static/media/Poppins-Regular.41e8dead.ttf"
+  },
+  {
+    "revision": "4c1ea5dcd1d67522db185f55030250b7",
+    "url": "/github-repository-viewer/static/media/background-github.4c1ea5dc.jpg"
+  },
+  {
+    "revision": "718cad629dbab1b10d8bb4858cf9d210",
+    "url": "/github-repository-viewer/static/media/github.718cad62.png"
+  }
+]);
